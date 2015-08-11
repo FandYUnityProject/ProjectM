077ef9f69b68abf439d74450b982682b5ff2f3e7
whitewhale6 <whitewhale6@gmail.com>
2015-08-11 16:09:30 +0900

    取得用

---
+++ ProjectM/Assets/FY/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	int floorMask;
+	float camRayLength =100f;
+
+	void FixedUpdate()
+	{
+		Vector3 targetCamPos = target.position;
+		transform.position = targetCamPos;
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		
+		RaycastHit floorHit;
+		
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) 
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+			
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			//Camera(newRotation);
+		}
+	}
+}
---
+++ ProjectM/Assets/FY/Scripts/MoveController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	public float h;
+	public float v;
+	public float speed;
+
+	private Rigidbody rb;
+
+	Vector3 movement;
+
+	int floorMask;
+	float camRayLength =100f;
+
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update (){
+		h = Input.GetAxisRaw ("Horizontal");
+		v = Input.GetAxisRaw("Vertical");
+
+		Move (h, v);
+		Turning ();
+
+	}
+
+	void Move(float h,float v)
+	{
+		movement.Set (h, 0f, v);
+		
+		movement = movement.normalized * speed * Time.deltaTime;
+		
+		rb.MovePosition (transform.position + movement);
+	}
+	
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		
+		RaycastHit floorHit;
+		
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) 
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+			
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			rb.MoveRotation(newRotation);
+		}
+	}
+}
